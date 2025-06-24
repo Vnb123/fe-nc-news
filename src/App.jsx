85d0e3836b3bf18bfa8312420bb89648c0c3e2d1
Vnb123 <vanabg@yahoo.com>
2025-06-24 09:53:40 +0000

    Set up the project with correct imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
-
-import "./App.css";
-
-function App() {}
+import { Routes, Route } from "react-router-dom";
+import Header from "./Header";
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes></Routes>
+    </>
+  );
+}
 
 export default App;
