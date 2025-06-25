357ad29ca944f75456be374a3ed65ea83eaa5f25
Vnb123 <vanabg@yahoo.com>
2025-06-25 14:13:22 +0000

    Added all comments to the article page

--- src/ArticleInfo.jsx
+++ src/ArticleInfo.jsx
@@ -1,16 +1,18 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import getArticleById from "./getArticleById";
+import getCommentsByArticleId from "./getCommentsByArticleId";
+
 function ArticleInfo() {
   const { article_id } = useParams();
   const [article, setArticle] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [comments, setComments] = useState([]);
 
   useEffect(() => {
     getArticleById(article_id)
       .then((articleObj) => {
-        console.log(articleObj);
         setArticle(articleObj.article);
         setIsLoading(false);
       })
@@ -19,6 +21,19 @@ function ArticleInfo() {
         setIsLoading(false);
       });
   }, [article_id]);
+
+  useEffect(() => {
+    getCommentsByArticleId(article_id)
+      .then((commentsObj) => {
+        setComments(commentsObj.comments);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setIsLoading(false);
+      });
+  }, [article_id]);
+
   if (isLoading) {
     return <p>Loading ◌</p>;
   }
@@ -35,7 +50,21 @@ function ArticleInfo() {
       <img src={article.article_img_url} alt="Article Image" />
       <p>{article.body}</p>
       <p>{article.votes} Votes</p>
-      <p>{article.comment_count} Comments</p>
+      <p>💬{article.comment_count} Comments</p>
+      <section id="comments">
+        <ul>
+          {comments.map((comment) => {
+            return (
+              <li id="comment" key={comment.comment_id}>
+                <p>{comment.author}</p>
+                <h3>Posted {new Date(comment.created_at).toLocaleString()}</h3>
+                <p>{comment.body}</p>
+                <p>Votes {comment.votes}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </main>
   );
 }
