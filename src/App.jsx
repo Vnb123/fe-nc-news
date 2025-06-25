d7a644d7910a16981add45f3a0ca35273b84ead0
Vnb123 <vanabg@yahoo.com>
2025-06-25 10:48:32 +0000

    Added a route for ArticleInfo page in the App

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import HomePage from "./HomePage";
+import ArticleInfo from "./ArticleInfo";
 function App() {
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/articles/:article_id" element={<ArticleInfo />} />
       </Routes>
     </>
   );
