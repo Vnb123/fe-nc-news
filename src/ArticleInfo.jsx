8ca22b5266ce5c0c5c36bd11d9e404736d33256d
Vnb123 <vanabg@yahoo.com>
2025-06-27 16:00:01 +0000

    Worked on making sure all the information of the user and date is displayed when posting a new comment. Debugging some errors. Added the option to delete users comments but working on having it load immediately instead of when refreshing and having more precise error handling

--- src/ArticleInfo.jsx
+++ src/ArticleInfo.jsx
@@ -4,7 +4,7 @@ import getArticleById from "./getArticleById";
 import getCommentsByArticleId from "./getCommentsByArticleId";
 import patchVotes from "./patchVotes";
 import postComment from "./postComment";
-
+import deleteComment from "./deleteComment";
 function ArticleInfo() {
   const { article_id } = useParams();
   const [article, setArticle] = useState([]);
@@ -15,6 +15,7 @@ function ArticleInfo() {
   const [input, setInput] = useState("");
   const [newCommentError, setNewCommentError] = useState(false);
   const [isPosted, setIsPosted] = useState(false);
+  const [deleteError, setDeleteError] = useState(false);
 
   useEffect(() => {
     getArticleById(article_id)
@@ -63,6 +64,7 @@ function ArticleInfo() {
 
   function handleSubmit(e) {
     e.preventDefault();
+
     const commentData = {
       username: "grumpy19",
       body: input,
@@ -70,14 +72,34 @@ function ArticleInfo() {
 
     postComment(article_id, commentData)
       .then((newComment) => {
+        if (!newComment.created_at) {
+          newComment.created_at = new Date().toISOString();
+        }
+        if (!newComment.author) {
+          newComment.author = commentData.username;
+        }
         setComments((currentComments) => [newComment, ...currentComments]);
         setInput("");
         setNewCommentError(false);
         setIsPosted(true);
       })
-      .catch(() => {
+      .catch((err) => {
         setNewCommentError(true);
         setIsPosted(false);
+      });
+  }
+  function handleDelete(comment_id) {
+    deleteComment(comment_id)
+      .then(() => {
+        setDeleteError(false);
+        setComments((currentComments) => {
+          return currentComments.filter(
+            (comment) => comment.comment_id !== comment_id
+          );
+        });
+      })
+      .catch(() => {
+        setDeleteError(true);
       });
   }
 
@@ -90,50 +112,62 @@ function ArticleInfo() {
 
   return (
     <main className="article-info">
-      <p>/{article.topic}</p>
       <h1>{article.title}</h1>
-      <h3>By {article.author}</h3>
-      <h3>{new Date(article.created_at).toLocaleString()}</h3>
-      <img src={article.article_img_url} alt="Article Image" />
-      <p>{article.body}</p>
-      <div id="vote">
-        <span>Vote</span>
-        <button type="submit" onClick={handleVoteIncrement}>
-          ⬆️
-        </button>
-        <button type="submit" onClick={handleVoteDecrement}>
-          ⬇️
-        </button>
-        <span>{article.votes}</span>
-        <p>{voteError ? "Voting failed" : ""}</p>
+      <div id="article-author-date">
+        <p>/{article.topic}</p>
+        <p>By {article.author}</p>
+        <p>{new Date(article.created_at).toLocaleString()}</p>
       </div>
-      <p>💬{article.comment_count} Comments</p>
-      <section className="adding-comment">
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="add-comment"></label>
-          <input
-            id="add-comment"
-            required
-            type="text"
-            onChange={handleInputChange}
-            value={input}
-          />
-          <button id="comment-submit" type="submit">
-            Comment
+      <div id="article-img-body">
+        <img src={article.article_img_url} alt="Article Image" />
+        <p>{article.body}</p>
+      </div>
+      <div id="article-vote-comment">
+        <div id="vote">
+          <span>Vote</span>
+          <button type="submit" onClick={handleVoteIncrement}>
+            ⬆️
           </button>
-          <p>{newCommentError ? "Failed to post" : ""}</p>
-          <p>{isPosted ? "Comment posted" : ""}</p>
-        </form>
-      </section>
+          <button type="submit" onClick={handleVoteDecrement}>
+            ⬇️
+          </button>
+          <span>{article.votes}</span>
+          <p>{voteError ? "Voting failed" : ""}</p>
+        </div>
+        <p>💬{article.comment_count} Comments</p>
+        <section className="adding-comment">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="add-comment"></label>
+            <input
+              id="add-comment"
+              required
+              type="text"
+              onChange={handleInputChange}
+              value={input}
+            />
+            <button id="comment-submit" type="submit">
+              Comment
+            </button>
+            <p>{newCommentError ? "Failed to post" : ""}</p>
+            <p>{isPosted ? "Comment posted" : ""}</p>
+          </form>
+        </section>
+      </div>
       <section id="comments">
         <ul>
           {comments.map((comment) => {
             return (
-              <li id="comment" key={comment.comment_id}>
+              <li className="comment" key={comment.comment_id}>
                 <p>{comment.author}</p>
                 <h3>Posted {new Date(comment.created_at).toLocaleString()}</h3>
                 <p>{comment.body}</p>
                 <p>Vote ⬆️⬇️ {comment.votes}</p>
+                {comment.author === "grumpy19" && (
+                  <button onClick={() => handleDelete(comment.comment_id)}>
+                    🗑️
+                  </button>
+                )}
+                <p>{deleteError ? "Failed to delete comment" : null}</p>
               </li>
             );
           })}
