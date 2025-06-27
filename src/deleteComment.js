1c138d3367cef749f5bf172cbe1735b20f1eee28
Vnb123 <vanabg@yahoo.com>
2025-06-27 15:58:10 +0000

    Created a function to fetch api to delete a comment

---
+++ src/deleteComment.js
@@ -0,0 +1,14 @@
+export default function deleteComment(comment_id) {
+  return fetch(`https://nc-news-ddg5.onrender.com/api/comments/${comment_id}`, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+  }).then((res) => {
+    if (!res.ok) {
+      return Promise.reject({
+        status: res.status,
+        msg: "Failed to delete comment",
+      });
+    }
+    return res.json();
+  });
+}
