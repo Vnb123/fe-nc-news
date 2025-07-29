85bd7c6b6f8e598f963b7082fe64e62772c1a05a
Vnb123 <vanabg@yahoo.com>
2025-07-29 21:22:01 +0000

    uninstalled tailwind from the project

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -7,12 +7,7 @@ export default function NotFound() {
         <h2>404</h2>
         <p>Page Not Found</p>
         <Link to="/">
-          <button
-            type="button"
-            // className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Back To Homepage
-          </button>
+          <button type="button">Back To Homepage</button>
         </Link>
       </div>
     </>
